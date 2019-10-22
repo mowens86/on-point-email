732863197d6e0a3e83911dfcfba03229d7666b0f
Mike Owens <mocoding86@gmail.com>
2019-10-21 17:12:06 -0700

    complete responsiveness for what we do section at 768px

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,17 +5,21 @@ window.onscroll = () => { stickyNavigation(); };
 
 // Get the navbar
 const navbar = document.getElementById("navbar");
+const fullnav = document.getElementById("fullnav");
 
 // Get the offset position of the navbar
-const sticky = navbar.offsetTop;
+const sticky = fullnav.offsetTop;
+
+
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 stickyNavigation = () => {
 
   // Is windows y-axis offset greater than or equal to navbar: true = add sticky class, false = remove sticky class 
-  window.pageYOffset >= sticky ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+  window.pageYOffset >= sticky ? fullnav.classList.add("sticky") : fullnav.classList.remove("sticky");
 
 };
+
 
 // Hamburger Bar
 const hamburgerBtn = document.querySelector(".hamburger");
@@ -43,4 +47,17 @@ function checkForViewportChange () {
         lastState = state;
 }}
 
+
+
 window.setInterval(checkForViewportChange, 150);
+
+// Check to see if top is 0 if so remove sticky navbar to reset padding
+console.log(document.body.scrollTop);
+let topState = false;
+function checkForTopOfPage () {
+  const topOfPage = document.body.scrollTop;
+  if (topOfPage === 0) {
+  return fullnav.classList.remove("sticky");
+}
+}
+
